5bbfde0c6e35b8f495a313443a3cf2080f97fa69
DJMarret <javier_dario_marret@hotmail.com>
2023-12-05 21:58:05 -0500

    update

--- intall.sh
+++ intall.sh
@@ -23,8 +23,8 @@ sudo apt install certbot python3-certbot-nginx -y
 
 sudo certbot --nginx -d ordenfacil.org -m dariomarret74@gmail.com --agree-tos --redirect
 
-sudo cp default.nginx /etc/nginx/sites-available/prod.xfiv.chat
-sudo ln -s /etc/nginx/sites-available/prod.xfiv.chat /etc/nginx/sites-enabled/prod.xfiv.chat
+sudo cp default.nginx /etc/nginx/sites-available/ordenfacil.org
+sudo ln -s /etc/nginx/sites-available/ordenfacil.org /etc/nginx/sites-enabled/ordenfacil.org
 
 
 sudo systemctl status certbot.timer
