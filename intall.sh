c2224c301a609009093a617d03b5177843642454
DJMarret <javier_dario_marret@hotmail.com>
2023-12-05 21:14:18 -0500

    first commit

---
+++ intall.sh
@@ -0,0 +1,34 @@
+# instalar node 18 y npm
+
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Instalar docker
+
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+
+# Instalar docker-compose
+
+sudo apt-get install -y docker-compose
+
+# Instalar nginx y certbot
+
+sudo apt-get install -y nginx
+
+sudo apt install certbot python3-certbot-nginx -y
+
+sudo certbot --nginx -d ordenfacil.org -m dariomarret74@gmail.com --agree-tos --redirect
+
+sudo cp default.nginx /etc/nginx/sites-available/prod.xfiv.chat
+sudo ln -s /etc/nginx/sites-available/prod.xfiv.chat /etc/nginx/sites-enabled/prod.xfiv.chat
+
+
+sudo systemctl status certbot.timer
+
+sudo certbot renew --dry-run
+
+sudo systemctl restart nginx
---
+++ up.sh
@@ -0,0 +1,2 @@
+docker login -u "djmarret1992" -p "Tumadre1@" docker.io
+docker-compose up -d && docker ps
