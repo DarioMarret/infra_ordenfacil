3bbbfedcd40380b736cd539b819fd7db3bb595a6
DJMarret <javier_dario_marret@hotmail.com>
2023-12-05 21:42:03 -0500

    update

--- ssl.sh
+++ ssl.sh
@@ -1,3 +1,8 @@
+# borrar los archivos de certificados anteriores
+sudo rm -rf /etc/letsencrypt/live/ordenfacil.org
+sudo rm -rf /etc/letsencrypt/archive/ordenfacil.org
+
+
 sudo cp default.nginx /etc/nginx/sites-available/ordenfacil.org
 sudo ln -s /etc/nginx/sites-available/ordenfacil.org /etc/nginx/sites-enabled/ordenfacil.org
 sudo systemctl restart nginx 
