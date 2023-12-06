33dea20363761ddb38e2d900add0cd1bf3391d45
DJMarret <javier_dario_marret@hotmail.com>
2023-12-05 21:29:59 -0500

    update

---
+++ ssl.sh
@@ -0,0 +1,3 @@
+sudo cp default.nginx /etc/nginx/sites-available/ordenfacil.org
+sudo ln -s /etc/nginx/sites-available/ordenfacil.org /etc/nginx/sites-enabled/ordenfacil.org
+sudo systemctl restart nginx
