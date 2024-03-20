cb609faa31cb1adca7f345aa68156bd86681aaee
DJMarret <javier_dario_marret@hotmail.com>
2024-03-20 11:10:40 -0500

    update

--- intall.sh
+++ intall.sh
@@ -23,8 +23,11 @@ sudo apt install certbot python3-certbot-nginx -y
 
 sudo certbot --nginx -d ordenfacil.org -m dariomarret74@gmail.com --agree-tos --redirect
 
+
+sudo certbot --nginx -d flashchat.chat -m dariomarret74@gmail.com --agree-tos --redirect
+
 sudo cp default.nginx /etc/nginx/sites-available/ordenfacil.org
-sudo ln -s /etc/nginx/sites-available/ordenfacil.org /etc/nginx/sites-enabled/ordenfacil.org
+sudo ln -s /etc/nginx/sites-available/flashchat.chat /etc/nginx/sites-enabled/flashchat.chat
 
 
 sudo systemctl status certbot.timer
